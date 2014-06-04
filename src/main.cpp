raffermeko/bitcoin

commit ce4069ff10e72525da4afc72221c4bdb334a8dd7

ProcessGetData(): abort if a block file is missing from disk

src/main.cpp
@@ -3373,7 +3373,7 @@ void static ProcessGetData(CNode* pfrom)
                 {
                     // Send block from disk
                     CBlock block;
-                    ReadBlockFromDisk(block, (*mi).second);
+                    assert(ReadBlockFromDisk(block, (*mi).second));
                     if (inv.type == MSG_BLOCK)
                         pfrom->PushMessage("block", block);
                     else // MSG_FILTERED_BLOCK)

commit a3c1c2407766c278b1c661b0c43eec83430a0c29

Remove side effect in assertion in ProcessGetData
A side-effect was introduced into an assertion in ce4069f. This commit
fixes that.

src/main.cpp
@@ -3390,7 +3390,8 @@ void static ProcessGetData(CNode* pfrom)
                 {
                     // Send block from disk
                     CBlock block;
-                    assert(ReadBlockFromDisk(block, (*mi).second));
+                    if (!ReadBlockFromDisk(block, (*mi).second))
+                        assert(!"cannot load block from disk");
                     if (inv.type == MSG_BLOCK)
                         pfrom->PushMessage("block", block);
                     else // MSG_FILTERED_BLOCK)